8717d6589fc3307501e79c17df3c95d625c26401
Ubuntu <ubuntu@ip-172-31-95-93.ec2.internal>
2019-09-20 18:40:56 +0000

    game.c game.gb index.html added

---
+++ game.c
@@ -0,0 +1,16 @@
+// Copyright (c) 2019 Amirmahdi Mersad All rights reserved
+//
+// Created by Amir Mersad
+// Created on September 2019
+// This program is the "Hello, World!" program for the GameBoy
+
+#include <stdio.h>
+#include <gb/gb.h>
+
+int main()
+{
+	printf("Hello, World! I'm   Amir");
+	
+	return 0;
+}
+	
